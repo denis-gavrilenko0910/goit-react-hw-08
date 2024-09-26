f8d58999b229ba40088a159de0a6a0a2fb403503
denis.g87 <denys.gavrylenko@itechhub.io>
2024-09-26 20:20:53 +0300

    fix: removed autocomplete

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -21,7 +21,7 @@ export const RegisterForm = () => {
     };
 
     return (
-        <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+        <form className={css.form} onSubmit={handleSubmit}>
             <label className={css.label}>
                 Username
                 <input type="text" name="name" />
